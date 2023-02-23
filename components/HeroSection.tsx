91254c40297a054f4988417a503ca2852ccfe5a2
Aditya <106834565+Aditarya@users.noreply.github.com>
2023-02-23 18:23:41 +0530

    Update HeroSection.tsx

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -21,10 +21,10 @@ const HeroSection = () => {
           <p className="text-lg mt-4 mb-6 md:text-2xl">
             I&#39;m a{" "}
             <span className="font-semibold text-teal-600">
-              Software Engineer{" "}
+              Web3 Engineer{" "}
             </span>
-            based in Los Angeles, CA. Working towards creating software that
-            makes life easier and more meaningful.
+            specializing in building (and occasionally designing) exceptional digital experiences. I enjoy creating things that live on the internet and that
+            makes our life easier and more meaningful.
           </p>
           <Link
             to="projects"
